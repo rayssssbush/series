bfc4929aa40044663ed35050546399af4bdfe975
ray__s <Rayy_Bush@proton.me>
2025-01-15 23:03:39 +0300

    tc_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024')
-const Serials = mongoose.model('Serial', { name: String })
+var schema = mongoose.Schema({ name: String })
+const Serials = mongoose.model('Serial', schema)
+schema.methods.ser = function () {
+	console.log(this.name + ' is very interesting')
+}
 const serial = new Serials({ name: 'Squid Game' })
 serial.save().then(() => console.log('Piu'))
