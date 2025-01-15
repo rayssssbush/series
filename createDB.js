803cde627d08c3283372f69e3d1a070a6d0aa78b
ray__s <Rayy_Bush@proton.me>
2025-01-15 22:29:30 +0300

    tc_6_2

--- createDB.js
+++ createDB.js
@@ -2,6 +2,8 @@ const { MongoClient } = require('mongodb')
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 // Connection URL
+var data = require('./data.js').data
+console.log(data)
 const url = 'mongodb://localhost:27017'
 const client = new MongoClient(url)
 // Database Name
---
+++ data.js
@@ -0,0 +1,34 @@
+var data = [
+	{
+		title: 'Squid Games',
+		nick: 'squid',
+		avatar: '/images/squid.jpeg',
+		desc: 'Южнокорейский сериал о смертельной игре на выживание, где участники борются за огромный денежный приз.',
+	},
+	{
+		title: 'Sherlock',
+		nick: 'sherlock',
+		avatar: '/images/sherlock.jpeg',
+		desc: 'Современная интерпретация приключений Шерлока Холмса и доктора Ватсона в Лондоне 21 века.',
+	},
+	{
+		title: 'Arcane',
+		nick: 'arcane',
+		avatar: '/images/arcane.jpg',
+		desc: 'Анимационный сериал, основанный на игре League of Legends, рассказывает историю города Пилтовера и его жителей.',
+	},
+	{
+		title: 'Loki',
+		nick: 'loki',
+		avatar: '/images/loki.jpeg',
+		desc: 'Сериал о боге обмана Локи, который оказывается вовлечён в многомировой кризис после событий фильма "Мстители: Финал".',
+	},
+	{
+		title: 'Supernatural',
+		nick: 'supernatural',
+		avatar: '/images/supernatural.jpeg',
+		desc: 'История двух братьев, Сэма и Дина Винчестеров, которые охотятся на демонов, духов и других сверхъестественных существ.',
+	},
+]
+
+module.exports.data = data
