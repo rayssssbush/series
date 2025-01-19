cb885f3f8edee6c4faae4b0e43cf4ed3b0532d10
ray__s <Rayy_Bush@proton.me>
2025-01-20 01:32:23 +0300

    tc_10_1

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express', counter: req.session.counter })
 })
 
+/* GET login/registration page. */
+router.get('/logreg', function (req, res, next) {
+	res.render('logreg', { title: 'Вход' })
+})
+
 module.exports = router
