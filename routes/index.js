02650c1790140ebca1cbf25399883efee5f90ee3
ray__s <Rayy_Bush@proton.me>
2025-01-20 01:12:28 +0300

    tc_9_4

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	req.session.greeting = 'Hi!!!' // Запись в сессию
-	res.render('index', { title: 'Express' })
+	req.session.greeting = 'Hi!!!'
+	res.render('index', { title: 'Express', counter: req.session.counter })
 })
 
 module.exports = router
