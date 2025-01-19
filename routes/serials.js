fbe74dd305b1dc4f24e99da43aa06ba41d25b140
ray__s <Rayy_Bush@proton.me>
2025-01-19 03:55:02 +0300

    tc_8_3

--- routes/serials.js
+++ routes/serials.js
@@ -1,11 +1,28 @@
 var express = require('express')
 var router = express.Router()
+var Serials = require('../models/serial').Serial
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 	res.send('Новый маршрутизатор, для маршрутов, начинающихся с serials')
 })
-router.get('/:nick', function (req, res, next) {
-	res.send(req.params.nick)
+router.get('/:nick', async function (req, res, next) {
+	try {
+		var serials = await Serial.find({ nick: req.params.nick })
+		console.log(serials)
+
+		if (!serials.length) {
+			return next(new Error('Нет такого сериала на сайте'))
+		}
+		var serial = serials[0]
+		res.render('serial', {
+			title: serial.title,
+			picture: serial.avatar,
+			desc: serial.desc,
+		})
+	} catch (err) {
+		console.error(err)
+		next(err)
+	}
 })
 
 module.exports = router
