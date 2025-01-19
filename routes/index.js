473b63dd371d95e51c283ac5024d0319936d9bc0
ray__s <Rayy_Bush@proton.me>
2025-01-19 05:06:11 +0300

    tc_9_2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Serials' })
+	res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express' })
 })
 
 module.exports = router
